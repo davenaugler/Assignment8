69d4345e68d1c266b6776773d07688c969789dea
Dave Naugler <davenaugler@gmail.com>
2023-11-20 16:45:30 -0700

    Optimizing thread pool size - and pushing it

--- src/main/java/com/coderscampus/assignment/GetData.java
+++ src/main/java/com/coderscampus/assignment/GetData.java
@@ -1,4 +1,5 @@
 package com.coderscampus.assignment;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -9,59 +10,90 @@ import java.util.stream.Collectors;
 
 public class GetData {
     public static void main(String[] args) throws InterruptedException, ExecutionException {
+        long startTime = System.currentTimeMillis(); // * START TIMER
 
-            Assignment8 assignment8 = new Assignment8();
-            List<CompletableFuture<List<Integer>>> futures = new ArrayList<>();
+        Assignment8 assignment8 = new Assignment8();
 
-            // Assuming getNumbers() needs to be called 1000 times
-            for (int count = 0; count < 1000; count++) {
-                CompletableFuture<List<Integer>> future = CompletableFuture.supplyAsync(assignment8::getNumbers);
-                futures.add(future);
-            }
+        // Optimize the thread pool size based on your system and task nature
+        int numberOfCores = Runtime.getRuntime().availableProcessors();
+        int poolSize = numberOfCores * 20; // Started out as... int poolSize = numberOfCores * 2;
+        ExecutorService executor = Executors.newFixedThreadPool(poolSize);
 
-            CompletableFuture<Void> allFutures = CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]));
-            CompletableFuture<List<List<Integer>>> allNumbersFuture = allFutures.thenApply(v -> futures.stream()
-                    .map(CompletableFuture::join)
-                    .collect(Collectors.toList())
-                    );
+        List<CompletableFuture<List<Integer>>> futures = new ArrayList<>();
 
-            // Get the combined result
-            List<List<Integer>> allNumbers = allNumbersFuture.get();
+        // Assuming getNumbers() needs to be called 1000 times
+//            for (int count = 0; count < 1000; count++) {
+//                CompletableFuture<List<Integer>> future = CompletableFuture.supplyAsync(assignment8::getNumbers, executor);
+//                futures.add(future);
+//            }
 
-            // Count the frequency of each number
-        Map<Integer, Integer> numberFrequencies = new HashMap<>();
+        for (int count = 0; count < 1000; count++) {
+            CompletableFuture<List<Integer>> future = CompletableFuture.supplyAsync(() -> {
+                try {
+                    return assignment8.getNumbers();
+                } catch (Exception e) {
+                    // Handle any exceptions here
+                    e.printStackTrace();
+                    return new ArrayList<Integer>(); // Return an empty list in case of error
+                }
+            }, executor);
+            futures.add(future);
+        }
+
+        CompletableFuture<Void> allFutures = CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]));
+        CompletableFuture<List<List<Integer>>> allNumbersFuture = allFutures.thenApply(v -> futures.stream()
+                .map(CompletableFuture::join)
+                .collect(Collectors.toList())
+        );
+
+        // Get the combined result
+        List<List<Integer>> allNumbers = allNumbersFuture.get();
+
+        // Count the frequency of each number
+        ConcurrentHashMap<Integer, Integer> numberFrequencies = new ConcurrentHashMap<>();
         for (List<Integer> numberList : allNumbers) {
             for (Integer number : numberList) {
-                numberFrequencies.put(number, numberFrequencies.getOrDefault(number, 0) + 1);
+                numberFrequencies.merge(number, 1, Integer::sum);
+//                numberFrequencies.put(number, numberFrequencies.getOrDefault(number, 0) + 1);
             }
         }
 
         // Output the frequencies
         numberFrequencies.forEach((number, frequency) -> {
-                    System.out.println("Number " + number + " appears " + frequency + " times");
-                });
+            System.out.println(number + " : " + frequency);
+        });
+
+        // Proper shutdown and handling of the executor
+        executor.shutdown();
+        if (!executor.awaitTermination(60, TimeUnit.SECONDS)) {
+            executor.shutdownNow();
+        }
+
+        long endTime = System.currentTimeMillis(); // * STOP TIMER
+//        long totalTime = endTime - startTime;
+
+        // * CALCULATE SECONDS
+        long totalTimeMillis = endTime - startTime;
+        long totalTimeSeconds = totalTimeMillis / 1_000;
+        long remainingMillis = totalTimeMillis % 1_000;
+
+//        System.out.println("Total execution time: " + totalTime + " milliseconds");
+        System.out.println("Total execution time: " + totalTimeSeconds + " seconds and " + remainingMillis + " milliseconds");
+        // ? Times below are WITHOUT multithreading
+        // Total execution time: 56 seconds and 531 milliseconds
+        // Total execution time: 56 seconds and 551 milliseconds
+
+        // ? Times below are WITH MULTITHREADING (.newFixedThreadPool(10);)
+        // Total execution time: 50 seconds and 562 milliseconds
+        // Total execution time: 50 seconds and 562 milliseconds
+
+        // ? Times below are WITH USER COMPUTER SPECS
+        // Total execution time: 25 seconds and 342 milliseconds
+        // Total execution time: 25 seconds and 343 milliseconds
+
+        // ? Times below are WITH USER COMPUTER SPECS * 20
+
 
     }
-//        Assignment8 assignment = new Assignment8();
-//        ExecutorService executor = Executors.newFixedThreadPool(10); // Using 10 threads for example
-//
-//        List<Future<List<Integer>>> futures = new ArrayList<>();
-//
-//        for (int i = 0; i < 1000; i++) {
-//            futures.add(executor.submit(assignment::getNumbers));
-//        }
-//
-//        executor.shutdown();
-//        executor.awaitTermination(1, TimeUnit.HOURS);
-//
-//        ConcurrentHashMap<Integer, Long> frequencyMap = new ConcurrentHashMap<>();
-//        for (Future<List<Integer>> future : futures) {
-//            List<Integer> numbers = future.get();
-//            for (Integer number : numbers) {
-//                frequencyMap.compute(number, (key, val) -> (val == null) ? 1 : val + 1);
-//            }
-//        }
-//
-//        frequencyMap.forEach((key, value) -> System.out.println(key + "=" + value));
-//    }
+
 }

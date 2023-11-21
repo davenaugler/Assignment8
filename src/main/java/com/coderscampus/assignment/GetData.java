01afe83374ee1a7dd6e4a89074014a1cab9ef2fd
Dave Naugler <davenaugler@gmail.com>
2023-11-20 17:44:46 -0700

    Refactored and reduced numberOfCores

--- src/main/java/com/coderscampus/assignment/GetData.java
+++ src/main/java/com/coderscampus/assignment/GetData.java
@@ -1,12 +1,12 @@
 package com.coderscampus.assignment;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
-
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingByConcurrent;
 
 public class GetData {
     public static void main(String[] args) throws InterruptedException, ExecutionException {
@@ -15,56 +15,65 @@ public class GetData {
         Assignment8 assignment8 = new Assignment8();
 
         int numberOfCores = Runtime.getRuntime().availableProcessors();
-        int poolSize = numberOfCores * 100;
+        int poolSize = numberOfCores * 8;
         ExecutorService executor = Executors.newFixedThreadPool(poolSize);
 
         List<CompletableFuture<List<Integer>>> futures = new ArrayList<>();
 
         for (int count = 0; count < 1000; count++) {
-            CompletableFuture<List<Integer>> future = CompletableFuture.supplyAsync(() -> {
-                try {
-                    return assignment8.getNumbers();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    return new ArrayList<>();
-                }
-            }, executor);
-            futures.add(future);
+            futures.add(createFuture(assignment8, executor));
         }
 
-        CompletableFuture<Void> allFutures = CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]));
-        CompletableFuture<List<List<Integer>>> allNumbersFuture = allFutures.thenApply(v -> futures.stream()
-                .map(CompletableFuture::join)
-                .collect(Collectors.toList())
-        );
+        List<List<Integer>> allNumbers = getAllNumbers(futures);
 
-        List<List<Integer>> allNumbers = allNumbersFuture.get();
+        ConcurrentMap<Integer, Long> numberFrequencies = allNumbers.parallelStream()
+                .flatMap(List::stream)
+                .collect(groupingByConcurrent(Function.identity(), counting()));
 
-        ConcurrentHashMap<Integer, Integer> numberFrequencies = new ConcurrentHashMap<>();
-        for (List<Integer> numberList : allNumbers) {
-            for (Integer number : numberList) {
-                numberFrequencies.merge(number, 1, Integer::sum);
+        numberFrequencies.forEach((number, frequency) -> System.out.println(number + " : " + frequency));
+
+        shutdownExecutor(executor);
+
+        printExecutionTime(startTime);
+    }
+
+    private static CompletableFuture<List<Integer>> createFuture(Assignment8 assignment8, ExecutorService executor) {
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                return assignment8.getNumbers();
+            } catch (RuntimeException e) {
+                e.printStackTrace();
+                return new ArrayList<>();
             }
-        }
+        }, executor);
+    }
 
-        numberFrequencies.forEach((number, frequency) -> {
-            System.out.println(number + " : " + frequency);
-        });
+    private static List<List<Integer>> getAllNumbers(List<CompletableFuture<List<Integer>>> futures) throws InterruptedException, ExecutionException {
+        CompletableFuture<Void> allFutures = CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]));
+        return allFutures.thenApply(v -> futures.stream()
+                        .map(CompletableFuture::join)
+                        .collect(Collectors.toList()))
+                .get();
+    }
 
+    private static void shutdownExecutor(ExecutorService executor) throws InterruptedException {
         executor.shutdown();
         if (!executor.awaitTermination(60, TimeUnit.SECONDS)) {
-            executor.shutdownNow();
+            List<Runnable> droppedTasks = executor.shutdownNow();
+            if (!droppedTasks.isEmpty()) {
+                System.err.println("Executor did not terminate in the specified time. " + droppedTasks.size() + " tasks were dropped.");
+            }
+            if (!executor.awaitTermination(60, TimeUnit.SECONDS)) {
+                System.err.println("Executor did not terminate after forceful shutdown.");
+            }
         }
+    }
 
-        long endTime = System.currentTimeMillis(); // * STOP TIMER
-
-        // * CALCULATE SECONDS
+    private static void printExecutionTime(long startTime) {
+        long endTime = System.currentTimeMillis();
         long totalTimeMillis = endTime - startTime;
         long totalTimeSeconds = totalTimeMillis / 1_000;
         long remainingMillis = totalTimeMillis % 1_000;
-
         System.out.println("Total execution time: " + totalTimeSeconds + " seconds and " + remainingMillis + " milliseconds");
-
     }
-
 }
